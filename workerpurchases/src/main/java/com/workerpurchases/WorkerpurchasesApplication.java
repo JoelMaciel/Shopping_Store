6ac21dca37939256f71a950704fd33b0cc91b092
Joel Maciel <soldado25461@gmail.com>
2022-10-11 09:14:05 -0300

    Created .workerpurchases

---
+++ workerpurchases/src/main/java/com/workerpurchases/WorkerpurchasesApplication.java
@@ -0,0 +1,13 @@
+package com.workerpurchases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerpurchasesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkerpurchasesApplication.class, args);
+    }
+
+}
