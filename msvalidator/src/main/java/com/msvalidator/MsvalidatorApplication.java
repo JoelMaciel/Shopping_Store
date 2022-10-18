d82a74e61e4942b95a87ad713bc4df737aafa989
Joel Maciel <soldado25461@gmail.com>
2022-10-18 18:32:21 -0300

    created microservice msvalidator

---
+++ msvalidator/src/main/java/com/msvalidator/MsvalidatorApplication.java
@@ -0,0 +1,13 @@
+package com.msvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvalidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvalidatorApplication.class, args);
+    }
+
+}
